6ae5d8a7dc266ac9d9aba7eb2121cfd1e2ac2860
Luca Beltrame <lbeltrame@kde.org>
2023-10-06 10:45:33 +0200

    Lint ASCAT.sc

--- bin/run_ascatsc.R
+++ bin/run_ascatsc.R
@@ -12,54 +12,54 @@ suppressPackageStartupMessages({
 parser <- arg_parser("Run ASCAT.sc", hide.opts = TRUE)
 
 parser <- add_argument(parser, "--tumor_bams",
-                       help = "Paths to the tumor BAMs (sorted and indexed)",
-                       nargs = Inf)
+    help = "Paths to the tumor BAMs (sorted and indexed)",
+    nargs = Inf)
 parser <- add_argument(parser, "--binsize", type = "integer",
-                       default = 30000,
-                       help = "Bin size in bp (default: 30000).")
+    default = 30000,
+    help = "Bin size in bp (default: 30000).")
 parser <- add_argument(parser, "--cpus", type = "integer", default = 1,
-                       help = "Number of cores to use.")
+    help = "Number of cores to use.")
 parser <- add_argument(parser, "--projectname", type = "string",
-                       default = "ASCATsc",
-                       help = "Flag to include in output file(s).")
+    default = "ASCATsc",
+    help = "Flag to include in output file(s).")
 parser <- add_argument(parser, "--build", type = "string",
-                       default = "hg38",
-                       help = "Genome to use")
+    default = "hg38",
+    help = "Genome to use")
 parser <- add_argument(parser, "--outdir",
-                       default = "./",
-                       help = "Destination directory to save data to")
+    default = "./",
+    help = "Destination directory to save data to")
 parser <- add_argument(parser, "--chromosomes",
-                       help = "Chromosomes to evaluate",
-                       default = "autosomes_x")
+    help = "Chromosomes to evaluate",
+    default = "autosomes_x")
 parser <- add_argument(parser, "--chrstring_bam", type = "string",
-                       default = "chr",
-                       help = "Chromosome prefix present in BAMs.")
+    default = "chr",
+    help = "Chromosome prefix present in BAMs.")
 parser <- add_argument(parser, "--sex", type = "string",
-                       default = "female",
-                       help = "Patient gender (default: female)")
+    default = "female",
+    help = "Patient gender (default: female)")
 parser <- add_argument(parser, "--segmentation_alpha", type = "float",
-                       default = 0.01,
-                       help = "Segmentation parameter for CBS.")
+    default = 0.01,
+    help = "Segmentation parameter for CBS.")
 parser <- add_argument(parser, "--predict_refit", type = "boolean",
-                       default = TRUE,
-                       help = "xgboost predictor to predict refitted ploidy \n
-                                and refit the profiles automatically")
+    default = TRUE,
+    help = "xgboost predictor to predict refitted ploidy \n
+            and refit the profiles automatically")
 parser <- add_argument(parser, "--max_tumor_ploidy", type = "float",
-                       default = 5,
-                       help = "Maximum tumor ploidy above which solutions \n
-                       will be masked")
+    default = 5,
+    help = "Maximum tumor ploidy above which solutions \n
+    will be masked")
 parser <- add_argument(parser, "--min-ploidy", type = "float",
-                       help = "Minimum ploidy to consider",
-                       default = 1.7)
+    help = "Minimum ploidy to consider",
+    default = 1.7)
 parser <- add_argument(parser, "--max-ploidy", type = "float",
-                       help = "Maximum ploidy to consider",
-                       default = 5)
+    help = "Maximum ploidy to consider",
+    default = 5)
 parser <- add_argument(parser, "--min-purity", type = "float",
-                       help = "Minimum purity to consider (fractional scale)",
-                       default = 0.05)
+    help = "Minimum purity to consider (fractional scale)",
+    default = 0.05)
 parser <- add_argument(parser, "--max-purity", type = "float",
-                       help = "Maximum purity to consider (fractional scale)",
-                       default = 1)
+    help = "Maximum purity to consider (fractional scale)",
+    default = 1)
 
 args <- parse_args(parser)
 
@@ -88,21 +88,23 @@ if (length(args$tumour_bams) > 1) {
 purities <- seq(args$min_purity, args$max_purity, 0.01)
 ploidies <- seq(args$min_ploidy, args$max_ploidy, 0.01)
 
-res <- run_sc_sequencing(tumour_bams = args$tumor_bams,
-                         allchr = allchr,
-                         sex = args$sex,
-                         binsize = args$binsize,
-                         chrstring_bam = args$chrstring_bam,
-                         purs = purities,
-                         ploidies = ploidies,
-                         maxtumourpsi = args$max_tumor_ploidy,
-                         build = args$build,
-                         MC.CORES = args$cpus,
-                         projectname = args$projectname,
-                         segmentation_alpha = args$segmentation_alpha,
-                         predict_refit = args$predict_refit,
-                         multipcf = multipcf,
-                         outdir = args$outdir)
+res <- run_sc_sequencing(
+    tumour_bams = args$tumor_bams,
+    allchr = allchr,
+    sex = args$sex,
+    binsize = args$binsize,
+    chrstring_bam = args$chrstring_bam,
+    purs = purities,
+    ploidies = ploidies,
+    maxtumourpsi = args$max_tumor_ploidy,
+    build = args$build,
+    MC.CORES = args$cpus,
+    projectname = args$projectname,
+    segmentation_alpha = args$segmentation_alpha,
+    predict_refit = args$predict_refit,
+    multipcf = multipcf,
+    outdir = args$outdir
+)
 
 # create segmentation dataframe
 
@@ -119,15 +121,15 @@ if (args$predict_refit == TRUE) {
 saveRDS(res, paste0(args$project, "_ASCAT.rds"))
 
 readr::write_tsv(df_summary, file = paste0(args$project, "_summary.txt"),
-                 quote = "needed")
+    quote = "needed")
 readr::write_tsv(df_final, file = paste0(args$project, "_segments.seg"),
-                 quote = "needed")
+    quote = "needed")
 
 df_gistic <- df_final %>%
     dplyr::select(chromosome, start, end, num.mark, logr) %>%
     dplyr::mutate(logr = round(as.numeric(logr), 5))
 
 readr::write_tsv(df_gistic, file = paste0(args$project, "_gistic.seg"),
-                 quote = "needed")
+    quote = "needed")
 
 message("Complete.")
