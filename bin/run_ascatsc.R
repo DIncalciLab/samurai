405c1ae98457f9f3c8be21eeb8c8ccc3ed107cfd
Luca Beltrame <luca.beltrame@humanitasresearch.it>
2023-11-03 14:48:45 +0100

    Add the sample column directly

--- bin/run_ascatsc.R
+++ bin/run_ascatsc.R
@@ -109,7 +109,8 @@ res <- run_sc_sequencing(
 # create segmentation dataframe
 df_final <- as.data.frame(ifelse(args$predict_refit,
         res[["allProfiles.refitted.auto"]],
-        res[["allProfiles"]]))
+        res[["allProfiles"]])) %>%
+    mutate(sample = res$summary$allSols$samplename)
 df_final$sample <- res$summary$allSols$samplename
 df_summary <- as.data.frame(ifelse(args$predict_refit,
         res$summary$allSols.refitted,
