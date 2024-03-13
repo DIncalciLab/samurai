6921d33cb81c40a27d8543e8c4d5850308a4e312
Luca Beltrame <luca.beltrame@humanitasresearch.it>
2024-03-13 11:23:01 +0100

    Linting fixes

--- bin/run_maftools.R
+++ bin/run_maftools.R
@@ -9,24 +9,24 @@ suppressPackageStartupMessages({
 parser <- arg_parser("Run Maftools", hide.opts = TRUE)
 
 parser <- add_argument(parser, "--all_lesions",
-                    help = "Paths to the GISTIC lesions file.",
-                    nargs = Inf)
+    help = "Paths to the GISTIC lesions file.",
+    nargs = Inf)
 parser <- add_argument(parser, "--amp_genes",
-                    help = "Paths to the GISTIC amplified genes file.",
-                    nargs = Inf)
+    help = "Paths to the GISTIC amplified genes file.",
+    nargs = Inf)
 parser <- add_argument(parser, "--del_genes",
-                    help = "Paths to the GISTIC deleted genes file.",
-                    nargs = Inf)
+    help = "Paths to the GISTIC deleted genes file.",
+    nargs = Inf)
 parser <- add_argument(parser, "--gistic_scores",
-                    help = "Paths to the GISTIC scores file.",
-                    nargs = Inf)
+    help = "Paths to the GISTIC scores file.",
+    nargs = Inf)
 
 args <- parse_args(parser)
 
 gistic_obj <- readGistic(gisticAllLesionsFile = args$all_lesions,
-                gisticAmpGenesFile = args$amp_genes,
-                gisticDelGenesFile = args$del_genes,
-                gisticScoresFile = args$gistic_scores)
+    gisticAmpGenesFile = args$amp_genes,
+    gisticDelGenesFile = args$del_genes,
+    gisticScoresFile = args$gistic_scores)
 
 png(filename="maftools_summary_mqc.png")
 gisticChromPlot(gistic = gistic_obj, markBands = "all")
