eca2b3ee258396ca3e4a4738078fde0f1deebf2a
SaraPotente <sara.potente@outlook.it>
2025-04-28 17:15:58 +0200

    fix R script

--- bin/run_hrdcna.R
+++ bin/run_hrdcna.R
@@ -42,9 +42,8 @@ message("Starting HRDCNA score analysis...")
 segments <- read_tsv(args$seg_file, show_col_types = FALSE)
 # Dataframe MUST have chr, start, end, segVal, sample
 segments$segVal <- round(segments$segVal, 0)
-# Perform HRD analysis
 # Perform HRD analysis based on genome build
-nmfcn_wgs <- if (args$genome == "hg38") {
+features <- if (args$genome == "hg38") {
   sigminercopy_hg38(data = segments, genome_build = "hg38")
 } else if (args$genome == "hg19") {
   sigminercopy(data = segments, genome_build = "hg19")
@@ -53,22 +52,22 @@ nmfcn_wgs <- if (args$genome == "hg38") {
 }
 
 message("Saving RDS with Sigminer features activity...")
-saveRDS(nmfcn_wgs, "hrdcna_features_activity.rds")
+saveRDS(features, "hrdcna_features_activity.rds")
 message("Sigminer features activity RDS has been saved...")
 
-readr::write_tsv(nmfcn_wgs, file = "hrdcna_features_activity.tsv", quote = "needed")
+readr::write_tsv(features, file = "hrdcna_features_activity.tsv", quote = "needed")
 
-score_wgs <- HRDprediction(data = nmfcn_wgs)
+hrdcna_score <- HRDprediction(data = features)
 
 # Assign HRD status based on threshold declared in the original paper
-score_wgs$hrd_status_predicted <- ifelse(score_wgs$HRDCNAScore >= args$hrdcna_threshold, "HRD+", "HRD-")
+hrdcna_score$hrd_status_predicted <- ifelse(hrdcna_score$HRDCNAScore >= args$hrdcna_threshold, "HRD+", "HRD-")
 # Order columns for multiqc
-score_wgs <- score_wgs %>% dplyr::select(sample, HRDCNAScore, hrd_status_predicted)
+hrdcna_score <- hrdcna_score %>% dplyr::select(sample, HRDCNAScore, hrd_status_predicted)
 
 message("Saving RDS with HRDCNA Scores...")
-saveRDS(score_wgs, "hrdcna_scores.rds")
+saveRDS(hrdcna_score, "hrdcna_scores.rds")
 message("HRDCNA Scores RDS has been saved...")
 
-readr::write_tsv(score_wgs, file = "hrdcna_summary_mqc.tsv", quote = "needed")
+readr::write_tsv(hrdcna_score, file = "hrdcna_summary_mqc.tsv", quote = "needed")
 
 message("Complete.")
